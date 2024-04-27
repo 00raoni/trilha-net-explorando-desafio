65c94c7871d4e977d5ebb17d4da060ed0ad804e8
raonisys3 <raoni.soares@sys3sistemas.com.br>
2024-04-27 10:31:34 -0300

    desafio

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,15 +17,11 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
-            {
+            if (hospedes.Count <= Suite.Capacidade)
                 Hospedes = hospedes;
-            }
             else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
+                throw new Exception($"Capacidade de {Suite.Capacidade.ToString()} excede o limite de {hospedes.Count.ToString()} hospede(s).");
+            // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido                            
         }
 
         public void CadastrarSuite(Suite suite)
@@ -36,8 +32,8 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // *IMPLEMENTE AQUI*            
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,14 +41,11 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = Suite.ValorDiaria * DiasReservados;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
+            valor = (DiasReservados >= 10) ? valor - (valor * (decimal) 0.1) : valor; 
 
             return valor;
         }
